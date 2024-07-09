aca5c69785096b330849c0ece2fc4bbfd06c6981
Juanky <juancamiloherrera960@gmail.com>
2024-07-09 00:59:03 -0500

    feat: Consumo Api
    Se consume la api de manera insegura temporalmente

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-// const urltest = "http://localhost:5182/api/folders";
+const urlIp = "http://192.168.1.7:5182/api/folders";
 
 // GREETINGS
 const date = new Date();
@@ -16,3 +16,28 @@ if (dateTime >= 0 && dateTime < 12) {
 
 greetings.textContent = `${dateName}, Juanky`;
 
+fetch(urlIp).then(r => r.json()).then((d) => {
+    d.forEach((folder) => {
+        const foldersDiv = document.querySelector(".folder-content");
+    
+        const folderItem = document.createElement("div");
+        folderItem.classList.add("folder-item")
+        foldersDiv.appendChild(folderItem);
+    
+        const folderIcon = document.createElement("div");
+        folderIcon.classList.add("folder-icon")
+        folderIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#ffffff" stroke="#ffffff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6a2 2 0 0 1 2-2h3.93a2 2 0 0 1 1.664.89l.812 1.22A2 2 0 0 0 13.07 7H19a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/></svg>`;
+        folderItem.appendChild(folderIcon);
+    
+        const folderName = document.createElement("div");
+        folderName.classList.add("folder-name");
+        
+        const folderNameP = document.createElement("p");
+        folderNameP.textContent = folder.name;
+        folderName.appendChild(folderNameP);
+
+        folderItem.appendChild(folderName);
+    })
+
+    console.log(d);
+})
